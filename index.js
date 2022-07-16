bd29a12de4440efb1a6a6ee23f971ca933bd8d9f
Zarivate <Zarivate1@gmail.com>
2022-07-16 13:30:33 -0500

    Finished initializing API

--- index.js
+++ index.js
@@ -8,3 +8,14 @@ const request = require("request-promise");
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+// Initial root route, the second parameters is the callback function that has our request and our response
+app.get("/", (req, res) => {
+  // When someone visit this url on the API, thsi message is what is sent back
+  res.send("Welcome to my first API");
+});
+
+// Start the server by having it listen to a port, thanks to express a callback function can also be called when the app starts running
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
