ecee3f6377fe09fb5213a8448fb7c4987dba66ea
Zarivate <Zarivate1@gmail.com>
2022-07-18 19:40:30 -0500

    Finished API routes/endpoints

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-// This contains all the API code
-
 // Express is the backend framework that will be used to create the API
 const express = require("express");
 const request = require("request-promise");
@@ -10,8 +8,10 @@ const app = express();
 // Initialization of the port(s) that will be used when running the server
 const PORT = process.env.PORT || 5000;
 
-const apiKey = process.env.AMAZON_API_KEY;
-const baseURL = `http://api.scraperapi.com?api_key=${apiKey}&autoparse=true`;
+// Since apiKeys should normally not be public, this function will instead take an API key and return a url
+// with that key as a parameter that can be used later on in the rest of the code
+const generateScraperUrl = (apiKey) =>
+  `http://api.scraperapi.com?api_key=${apiKey}&autoparse=true`;
 
 app.use(express.json());
 
@@ -21,17 +21,70 @@ app.get("/", (req, res) => {
   res.send("Welcome to my first API");
 });
 
-// Route for getting product details. The colon here means that "productId" will be dynamic
+// Route for GETting product details. The colon here means that "productId" will be dynamic and all the urls are based off the actual Amazon urls query params
 app.get("/products/:productId", async (req, res) => {
   // The productId will be populated inside of req.params
   const { productId } = req.params;
+  const { api_Key } = req.query;
 
   try {
     const response = await request(
-      `${baseURL}&url=https://www.amazon.com/dp/${productId}`
+      `${generateScraperUrl(
+        api_Key
+      )}&url=https://www.amazon.com/dp/${productId}`
     );
+    res.json(JSON.parse(response));
+  } catch (error) {
+    res.json(error);
+  }
+});
 
-    res.json(response);
+// Another route/endpoint, this time for GETting the reviews of the item
+app.get("/products/:productId/reviews", async (req, res) => {
+  const { productId } = req.params;
+  const { api_Key } = req.query;
+
+  try {
+    const response = await request(
+      `${generateScraperUrl(
+        api_Key
+      )}&url=https://www.amazon.com/product-reviews/${productId}`
+    );
+    res.json(JSON.parse(response));
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+// Route/endpoint for GETting the product offers
+app.get("/products/:productId/offers", async (req, res) => {
+  const { productId } = req.params;
+  const { api_Key } = req.query;
+
+  try {
+    const response = await request(
+      `${generateScraperUrl(
+        api_Key
+      )}&url=https://www.amazon.com/gp/offer-listing/${productId}`
+    );
+    res.json(JSON.parse(response));
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+// GET search results for any product
+app.get("/search/:searchQuery", async (req, res) => {
+  const { searchQuery } = req.params;
+  const { api_Key } = req.query;
+
+  try {
+    const response = await request(
+      `${generateScraperUrl(
+        api_Key
+      )}&url=https://www.amazon.com/s?k=${searchQuery}`
+    );
+    res.json(JSON.parse(response));
   } catch (error) {
     res.json(error);
   }
